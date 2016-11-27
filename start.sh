d3f3ff935ecbcbb34944be48d9583e657de2e84e
Tom <tom@mdashx.com>
2016-11-27 14:17:46 +0000

    Install aptitiude and curl in start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+sudo apt-get install aptitude
+sudo apt-get install curl
